958fa726fc18e0e5e82d7657a2cbc245219e112a
MrEvill <ppiskorz488@gmail.com>
2017-01-11 10:08:22 +0100

    Add files via upload

---
+++ DodajPrzepis.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Cukiernia_WPF
+{
+    partial class DodajPrzepis
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.aaa = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(183, 153);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(75, 23);
+            this.btnDodaj.TabIndex = 0;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            this.btnDodaj.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // aaa
+            // 
+            this.aaa.AutoSize = true;
+            this.aaa.Location = new System.Drawing.Point(99, 70);
+            this.aaa.Name = "aaa";
+            this.aaa.Size = new System.Drawing.Size(85, 13);
+            this.aaa.TabIndex = 1;
+            this.aaa.Text = "Nazwa przepisu:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Czas przyg. w min.:";
+            // 
+            // DodajPrzepis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.aaa);
+            this.Controls.Add(this.btnDodaj);
+            this.Name = "DodajPrzepis";
+            this.Size = new System.Drawing.Size(294, 214);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Label aaa;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ DodajPrzepis.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cukiernia_WPF
+{
+    public partial class DodajPrzepis : UserControl
+    {
+        public DodajPrzepis()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
